dd32e322ee602796bd24f69a3e018f44dd0bfa18
user42 <clucien@42virtualBox.fr>
2021-06-30 17:08:13 +0200

    scroll detection

--- srcs/hook.c
+++ srcs/hook.c
@@ -2,17 +2,17 @@
 
 #include "fractol.h"
 
-int	hk_key_press(int key, void *ptr)
+int	hk_scroll_up(int key, void *ptr)
 {
 	(void)ptr;
 	printf("press: %d\n", key);
 	return (1);
 }
 
-int	hk_key_release(int key, void *ptr)
+int	hk_scroll_down(int key, void *ptr) // ?
 {
 	(void)ptr;
-	printf("release: %d\n", key);
+	printf("press: %d\n", key);
 	return (1);
 }
 
